96abc6de7663eca4f05e0e6a5abd518a49e0bcc3
Li2thX <danylo.medvid.ki.2021@lpnu.ua>
2025-05-06 16:51:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,3 +22,8 @@ app.get('/get-invite', async (req, res) => {
 app.listen(3000, () => {
   console.log('Сервер запущено на порту 3000');
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущено на порту ${PORT}`);
+});
